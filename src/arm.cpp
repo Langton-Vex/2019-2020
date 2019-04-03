871e772984bbc714e1fb2068339543aa6a2d81dc
Josh <vex@thelangton.org.uk>
2019-04-03 08:45:21 +0100

    mild changes

--- src/arm.cpp
+++ src/arm.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
 Arm::Arm(){
-  peripherals.arm_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  peripherals.arm_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
 }
 
 void Arm:: user_control(){
