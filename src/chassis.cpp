6f2d5f5e067e04db9a9853f4dd214af8b21dce7b
Josh <vex@thelangton.org.uk>
2019-09-05 14:41:23 +0100

    Changed to quad motor drive

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -2,87 +2,35 @@
 #include "math.h"
 #include "stdexcept"
 
-//#define PI 3.14159
-#define IN_TO_CM 2.54 // MULTIPLY BY THIS TO CONVERT INCHES TO CM
 
-Chassis::Chassis(double t_r, double w_c){
-	// H-Drive
-
-	turn_radius = t_r;
-	wheel_circumference = w_c;
-
-	pros::motor_gearset_e_t tank_motor_gearset = peripherals.left_mtr.get_gearing();
-	if (tank_motor_gearset == MOTOR_GEARSET_06) tank_motor_speed = 600;
-	else if (tank_motor_gearset == MOTOR_GEARSET_18) tank_motor_speed = 200;
-	else if (tank_motor_gearset == MOTOR_GEARSET_36) tank_motor_speed = 100;
-	else throw std::invalid_argument("Cannot get gearset of left mtr");
-
-	pros::motor_gearset_e_t strafe_motor_gearset = peripherals.strafe_mtr.get_gearing();
-	if (strafe_motor_gearset == MOTOR_GEARSET_06) tank_motor_speed = 600;
-	else if (strafe_motor_gearset == MOTOR_GEARSET_18) tank_motor_speed = 200;
-	else if (strafe_motor_gearset == MOTOR_GEARSET_36) tank_motor_speed = 100;
-	else throw std::invalid_argument("Cannot get gearset of left mtr");
+Chassis::Chassis(){
+  pros::motor_gearset_e_t motor_gearset = peripherals.left_mtr.get_gearing();
+  if (motor_gearset == MOTOR_GEARSET_06) motor_speed = 600;
+  else if (motor_gearset == MOTOR_GEARSET_18) motor_speed = 200;
+  else if (motor_gearset == MOTOR_GEARSET_36) motor_speed = 100;
+  else throw std::invalid_argument("Cannot get gearset of left mtr");
 }
 
 void Chassis:: user_control(){
-	int forward = peripherals.master_controller.get_analog(ANALOG_RIGHT_Y);
-	int right = peripherals.master_controller.get_analog(ANALOG_RIGHT_X);
-  int turn = peripherals.master_controller.get_analog(ANALOG_LEFT_X);
+      int power = peripherals.master_controller.get_analog(ANALOG_RIGHT_Y);
+      int turn = peripherals.master_controller.get_analog(ANALOG_RIGHT_X);
 
-	this->set(forward,right,turn);
+      this->set(power,turn);
 }
 
 
-void Chassis::set(int forward, int right, int turn){
+void Chassis::set(int power, int turn){
 
-	//float powere = 1/(sgn(power) * 127) * pow((float)power,2); // exponential voltage function
-	//float turne = 1/(sgn(turn) * 127) * pow((float)turn,2);
-	float powere = (sgn(forward) / tank_motor_speed) * pow(((float)forward*tank_motor_speed / 127),2); // exponential speed function
-	float righte = (sgn(right) / strafe_motor_speed) * pow(((float)right*strafe_motor_speed / 127),2);
-	float turne = (sgn(turn) / tank_motor_speed) * pow((float) (turn*tank_motor_speed / 127),2);
+  //float powere = 1/(sgn(power) * 127) * pow((float)power,2); // exponential voltage function
+  //float turne = 1/(sgn(turn) * 127) * pow((float)turn,2);
 
-	int left_speed = (int) round(powere) + (int) round(turne);
-	int right_speed = (int) round(powere) - (int) round(turne);
-	int strafe_speed = (int) round(righte);
-
-	peripherals.left_mtr.move_velocity(left_speed);
-	peripherals.right_mtr.move_velocity (right_speed);
-	peripherals.strafe_mtr.move_velocity (strafe_speed);
+  float powere = (sgn(power) / motor_speed) * pow(((float)power*motor_speed / 127),2); // exponential speed function
+  float turne = (sgn(turn) / motor_speed) * pow((float) (turn*motor_speed / 127),2);
+  int left = (int) powere + (int) turne;
+  int right = (int) powere - (int) turne;
+  pros::lcd::print(0, "Left: %d\nRight: %d\n", left,right);
+  peripherals.left_mtr.move_velocity(left);
+  peripherals.right_mtr.move_velocity (right);
+  peripherals.lefttwo_mtr.move_velocity(left);
+  peripherals.righttwo_mtr.move_velocity ( right);
 }
-
-/*
-distance: a float, in metres
-velocity: an integer, in RPM
-void Chassis::move_forward(double distance,int velocity=100){
-	double rel_target = distance / wheel_circumference;
-	double abs_target = peripherals.left_mtr.get_position() + rel_target;
-  pros::lcd::print(0,"%f",rel_target);
-  peripherals.left_mtr.move_relative(100,100);
-  peripherals.right_mtr.move_relative(100,100);
-	//peripherals.left_mtr.move_relative(rel_target,velocity);
-	//peripherals.right_mtr.move_relative(rel_target,velocity);
-
-	while (!((peripherals.left_mtr.get_position() < abs_target + 5.0) &&
-				   (peripherals.left_mtr.get_position() > abs_target - 5.0))) {
-	  // Continue running this loop as long as the motor is not within +-5 units of its goal
-	  pros::delay(20);
-  }
-
-};
-
-angle: an int, in degrees
-velocity: an integer, in RPM
-void Chassis::point_turn(double angle,int velocity=100){
-	double rel_target = (2*turn_radius*PI * (angle/360)) / wheel_circumference;
-	double abs_target = peripherals.left_mtr.get_position() + rel_target;
-	peripherals.left_mtr.move_relative(rel_target,velocity);
-	peripherals.right_mtr.move_relative(-rel_target,velocity);
-
-	while (!((peripherals.left_mtr.get_position() < abs_target + 5.0) &&
-				   (peripherals.left_mtr.get_position() > abs_target - 5.0))) {
-	  // Continue running this loop as long as the motor is not within +-5 units of its goal
-	  pros::delay(2);
-  }
-
-};
-*/
--- src/peripherals.cpp
+++ src/peripherals.cpp
@@ -1,8 +1,10 @@
 #include "main.h"
 
 
-Peripherals_t::Peripherals_t(int left, int right, int strafe) :
+Peripherals_t::Peripherals_t(int left, int right, int lefttwo, int righttwo) :
   left_mtr( left, MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS),
   right_mtr(right, MOTOR_GEARSET_18,true, MOTOR_ENCODER_ROTATIONS),
-  strafe_mtr(strafe, MOTOR_GEARSET_36,true, MOTOR_ENCODER_ROTATIONS),
+  lefttwo_mtr( lefttwo, MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS),
+  righttwo_mtr(righttwo, MOTOR_GEARSET_18,true, MOTOR_ENCODER_ROTATIONS),
+
   master_controller(pros::E_CONTROLLER_MASTER){};
