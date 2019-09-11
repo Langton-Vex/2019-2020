b37a82bc409e4ec6253bea4468d1340a84fe6914
Josh <vex@thelangton.org.uk>
2019-09-11 08:46:26 +0100

    this errors

--- include/project/arm.h
+++ include/project/arm.h
@@ -12,17 +12,19 @@ class Arm{
     void set(int power);
     void set_pos(double position);
 
-    int current_major_position;
-    int current_minor_position;
+    unsigned int current_major_position;
+    unsigned int current_minor_position;
 
-    double major_positions[4] = {0.0,1.0,1.5,2.0};
-    double minor_positions[4] = {0.0,0.2,0.4,0.6};
+    double major_positions[4] = {0.0,0.5,1,1.5};
+    double minor_positions[4] = {0.0,0.1,0.2,0.2};
 
     double user_pos_modifier;
     double sensitivity;
   protected:
     //Peripherals_t peripherals;
     int current_power;
+    int L1_last = 0;
+    int L2_last = 0;
 
 
 };
--- src/arm.cpp
+++ src/arm.cpp
@@ -10,7 +10,7 @@ Arm::Arm(){
   current_major_position = 0;
   current_minor_position = 0;
   user_pos_modifier = 0;
-  sensitivity = 1;
+  sensitivity = 0.0001;
 }
 
 void Arm:: user_control(){
@@ -18,38 +18,42 @@ void Arm:: user_control(){
       //int pos_joy = peripherals.master_controller.get_analog(ANALOG_LEFT_Y);
       //float pos_rotations = (float)pos_joy / (127 * 4); // figure out the bounds of the arm,
                                                          //  and set appropriate values
-
-      int arm_up = peripherals.master_controller.get_digital_new_press(DIGITAL_L1);
-      int arm_down = peripherals.master_controller.get_digital_new_press(DIGITAL_L2);
-
+      bool arm_up = (!L1_last and peripherals.master_controller.get_digital(DIGITAL_L1)) ?
+        true:false;
+      bool arm_down = (!L2_last and peripherals.master_controller.get_digital(DIGITAL_L2)) ?
+          true:false;
+      L1_last = peripherals.master_controller.get_digital(DIGITAL_L1);
+      L2_last = peripherals.master_controller.get_digital(DIGITAL_L2);
       int block_up = peripherals.master_controller.get_digital_new_press(DIGITAL_R1);
       int block_down = peripherals.master_controller.get_digital_new_press(DIGITAL_R2);
 
       current_major_position = arm_up&(current_major_position <= 3) ?
           current_major_position+1 : current_major_position;
-      current_major_position = arm_down&(current_major_position >= 0) ?
+      current_major_position = arm_down&(current_major_position > 0) ?
           current_major_position-1 : current_major_position;
 
-      current_minor_position = block_up&(current_minor_position <= 3) ?
+      current_minor_position = block_up&(current_minor_position <= 3 ) ?
           current_minor_position+1 : current_minor_position;
-      current_minor_position = block_down&(current_minor_position >= 0) ?
+      current_minor_position = block_down&(current_minor_position > 0) ?
           current_minor_position-1 : current_minor_position;
 
-      user_pos_modifier += (double)power * sensitivity;
+      //user_pos_modifier += (double)power * sensitivity;
 
 
       double final_height = major_positions[current_major_position] +
-                    minor_positions[current_minor_position] +
-                    user_pos_modifier;
+                    minor_positions[current_minor_position];
 
-      if (final_height < max_height)
-        this->set_pos(final_height);
+      //double final_height = user_pos_modifier;
+      //if (final_height < max_height)
+
 
       // Quick and dirty place to put this
-      std::string arm_pos = std::to_string(peripherals.rightarm_mtr.get_position());
+      std::string arm_pos = std::to_string(final_height);
       pros::lcd::set_text(1,arm_pos);
+      if (power > 5 || power < 5)
+        this->set(power);
 
-      //this->set(power);
+      //this->set_pos(final_height);
 }
 
 void Arm::set(int power){
@@ -59,6 +63,7 @@ void Arm::set(int power){
 
 }
 void Arm::set_pos(double position){
+  /*
     if (position < 0.1) {
       peripherals.leftarm_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
       peripherals.rightarm_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
@@ -67,7 +72,7 @@ void Arm::set_pos(double position){
       peripherals.leftarm_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
       peripherals.rightarm_mtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
       }
-
-    peripherals.leftarm_mtr.move_absolute(position,63);
-    peripherals.rightarm_mtr.move_absolute(position,63);
+    */
+    peripherals.leftarm_mtr.move_absolute(-position,63);
+    peripherals.rightarm_mtr.move_absolute(-position,63);
 }
