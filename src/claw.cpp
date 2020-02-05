fe11f1aeeae87d53b7a5ea628e3180c8add758a6
Langton-Vex <vex@thelangton.org.uk>
2020-02-05 16:47:43 +0000

    claw macro

--- src/claw.cpp
+++ src/claw.cpp
@@ -10,6 +10,19 @@ Claw::Claw() {
     peripherals->intake_mtr.set_brake_mode(MOTOR_BRAKE_COAST);
 }
 
+void one_drop(){
+     const int drop_time = 66;
+     const int grab_time = 20;
+     peripherals->intake_mtr.set_brake_mode(MOTOR_BRAKE_HOLD);
+     peripherals->intake_mtr.move_velocity(200);
+     pros::delay(grab_time);
+     peripherals->intake_mtr.move_velocity(0);
+     pros::delay(drop_time - grab_time); // 66 is the magic number from suvat
+     peripherals->intake_mtr.move(-127);
+     pros::delay(50);
+     peripherals->intake_mtr.set_brake_mode(MOTOR_BRAKE_COAST);
+}
+
 void Claw::user_control() {
     int intake = 0;
     int eject = 0;
@@ -24,6 +37,10 @@ void Claw::user_control() {
         power = 127;
     else
         power = 0;
+
+    if ( peripherals->master_controller.get_digital(DIGITAL_RIGHT)){
+        one_drop();
+    }
 
     this->set(power);
 }
